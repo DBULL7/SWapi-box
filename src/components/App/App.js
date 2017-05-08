24f659f56e75cace66d3e14e956899965c645560
Devin Dixon <devinisafoal@gmail.com>
2017-05-08 13:56:00 -0600

    initial structure of view favorites component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Favorites from '../Favorites/Favorites'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <section className='header'>
+          <h1>SWAPI-Box</h1>
+          <Favorites />
+        </section>
       </div>
     );
   }
---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Favorites = () => {
+  return (
+    <button className='view-favorites'>
+      View Favorites
+    </button>
+  )
+}
+
+Favorites.propTypes = {
+
+}
+
+export default Favorites;
