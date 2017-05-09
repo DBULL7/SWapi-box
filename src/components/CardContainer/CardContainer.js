b864e5be46cf7369f222017bf45bb8ef56551e41
Devon Bull <dbull@live.com>
2017-05-08 18:16:47 -0600

    imported PropTypes, Card name is now rendering

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 
-const CardContainer = () => {
-  return (
-    <div>
-      I am the card container
-    </div>
-  )
+const CardContainer = ( { cardData } ) => {
+  if (cardData) {
+    const cards = cardData.map(card => {
+      return (
+        <p>{card.name}</p>
+      )
+    })
+    return (
+      <div>
+        {cards}
+      </div>
+    )
+  }
+  }
+
+
+CardContainer.propTypes = {
+  cardData: PropTypes.array
 }
 
 export default CardContainer;
